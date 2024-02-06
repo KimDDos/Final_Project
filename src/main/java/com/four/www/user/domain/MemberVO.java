c6e5677e7a181025a9e771061099bfda3a0ebaef
Doyeon Kim <ehdus0731@daum.net>
2024-02-06 22:22:12 +0900

    ㅁㄴㅇㅁㄴㅇ

--- src/main/java/com/four/www/user/domain/MemberVO.java
+++ src/main/java/com/four/www/user/domain/MemberVO.java
@@ -1,5 +1,16 @@
 package com.four.www.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberVO {
 
 	private String userSerialNo;
