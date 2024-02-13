d782dbdd02f2fa4a88ae96eec1dac5135dbd6c22
DESKTOP-ABK77D4\EZENIC-158 <ehdus0731@gmail.com>
2024-02-13 15:04:51 +0900

    OAuth 구현중 2일차

--- src/main/java/com/four/www/user/oauth/loginController.java
+++ src/main/java/com/four/www/user/oauth/loginController.java
@@ -1,9 +1,11 @@
 package com.four.www.user.oauth;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,35 +14,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.four.www.user.domain.MemberVO;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping("/login/**")
 @CrossOrigin(origins = "*")
-@PropertySource("classpath:application-api.properties")
+@RequiredArgsConstructor
 public class loginController {
 	
-	@Value("${google.client.id}")
-	private String googleClientId;
-	@Value("${google.client.pwd}")
-	private String googleClientPwd;
-	@Value("${redirect.url}")
-	private String redirect_uri;
+	private final OauthParser parser;
+	
+	private final GoogleOAuth googleOAuth;
 	
 	@GetMapping("/google")
 	public void getGoogleAuthUrl(HttpServletResponse response) throws Exception {
-		response.sendRedirect("https://accounts.google.com/o/oauth2/v2/auth?"
-				+ "scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&"
-				+ "response_type=code&"
-				+ "redirect_uri="+redirect_uri
-				+ "&client_id="+googleClientId);
+		// 구글 Code 발행을 위한 유저 구글 로그인을 위한 로그인 사이트로 이동
+		response.sendRedirect(googleOAuth.getGoogleAuthUrl());
 	}
 	
 	@GetMapping(value = "/{socialLoginType}/callback")
-	public String callback(@PathVariable(name = "socialLoginType") String socialLoginType ,
+	public String getAccessToken(@PathVariable(name = "socialLoginType") String socialLoginType ,
 			@RequestParam(name="code") String code) {
 		log.info(">>>> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
-		return "";
+		
+		Map<String, String> userInfo = googleOAuth.getGoogleAccessTokenUrl(code);
+		
+		
+		
+		switch (socialLoginType) {
+		case "google":
+			 
+			try {
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			break;
+		default:
+			break;
+		}
+		
+		return "redirect:/";
 	}
 }
