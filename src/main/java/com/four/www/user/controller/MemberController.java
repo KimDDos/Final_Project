2370a979cd877187425095b2dd3c5d17b8ab1102
Bitmul <wlgns7884@gmail.com>
2024-02-14 15:20:39 +0900

    캘린더 수정

--- src/main/java/com/four/www/user/controller/MemberController.java
+++ src/main/java/com/four/www/user/controller/MemberController.java
@@ -1,14 +1,20 @@
 package com.four.www.user.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.http.HttpServletRequest;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.four.www.user.domain.CalendarVO;
 import com.four.www.user.domain.MemberVO;
 import com.four.www.user.service.MemberService;
 
@@ -20,51 +26,75 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member/**")
 @RequiredArgsConstructor
 public class MemberController {
-	
+
 	private final MemberService msv;
-	
+
 	@GetMapping("/memberRegister")
-	public void memberRegister() {}
-	
+	public void memberRegister() {
+	}
+
 	@GetMapping("/mypage")
-	public void mypage() {}
-	
+	public void mypage() {
+	}
+
 	@GetMapping("/calendar")
-	public void calendar() {}
-	
+	public void calendar() {
+	}
+
+	@GetMapping("/calendarRegister")
+	public void calendarRegister(@RequestParam(value = "date", required = false) String date, Model m) {
+		m.addAttribute("datedata", date);
+	}
+
+	@PostMapping("/calendarRegister")
+	public String calendarRegister(@RequestParam(value = "date", required = false) String date, CalendarVO CalVO,
+			String Start, String End) {
+		log.info("START : " + Start + "\nEnd :" + End);
+		String dateStr = date + " " + Start;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		LocalDateTime datetime = LocalDateTime.parse(dateStr,formatter);
+		CalVO.setCalScheduleStartTime(datetime);
+		dateStr = date + " " + End;
+		formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		datetime = LocalDateTime.parse(dateStr, formatter);   
+		CalVO.setCalScheduleEndTime(datetime);
+		log.info(CalVO.toString());
+		return "/member/calendar";
+	}
+
 	@GetMapping("/coupon")
-	public void coupon() {}
-	
-	//mypage에서 사용하는 modify.
+	public void coupon() {
+	}
+
+	// mypage에서 사용하는 modify.
 	@GetMapping("/mypageModify")
 	public void mypageModify() {
 	}
-	
-	
-	//mypage에서 사용하는 modify.
+
+	// mypage에서 사용하는 modify.
 	@PostMapping("/memberModify")
 	public String memberModify(MemberVO mvo) {
 		log.info(">>>> mvo >>>> {}", mvo);
-		
+
 		return "redirect:/member/mypage";
 	}
-	
+
 	@PostMapping("/memberRegister")
 	public String memberRegister(MemberVO mvo) {
 		log.info(">>>> mvo >>>> {}", mvo);
-		
+
 		int isOk = msv.memberRegister(mvo);
-		
+
 		return "/";
 	}
-	
+
 	@GetMapping("/memberLogin")
-	public void memberLogin() {}
-	
-	
+	public void memberLogin() {
+	}
+
 	@PostMapping("/memberLogin")
 	public String memberLogin(HttpServletRequest request, RedirectAttributes re) {
-		
+
 		re.addFlashAttribute("userEmail", request.getAttribute("userEmail"));
 		re.addFlashAttribute("errMsg", request.getAttribute("errMsg"));
 		return "redirect:/member/login";
