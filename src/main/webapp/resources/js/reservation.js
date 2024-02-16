f6e1eba7d8c0cb0b5fe712bb11126d2d4c6311f0
Bitmul <wlgns7884@gmail.com>
2024-02-16 15:18:06 +0900

    js코드 간결화

--- src/main/webapp/resources/js/reservation.js
+++ src/main/webapp/resources/js/reservation.js
@@ -1,22 +1,8 @@
-let checkBoxes = document.querySelectorAll('#checkBox');
-let checklist = [];
-for (let i =0; i<checkBoxes.length;i++)
-{
-    checklist[i] = 0;
-    console.log(checklist);
-}
-checkBoxes.forEach((element,index)=> {
-    element.addEventListener('click',(e)=>{
-        console.log('click' + ' ' + index);
-        if (checklist[index] == 0) 
-        {
-            checklist[index] = 1;
-            e.target.previousElementSibling.src = "/resources/img/circle-fill.svg";
-        }
-        else if (checklist[index] == 1) 
-        {
-            checklist[index] = 0;
-            e.target.previousElementSibling.src = "/resources/img/circle.svg";
-        }
-    })
+const checkBoxes = document.querySelectorAll('#checkBox');
+checkBoxes.forEach((checkBox, index) => {
+    let isChecked = false;
+    checkBox.addEventListener('click', () => {
+        isChecked = !isChecked;
+        checkBox.previousElementSibling.src = isChecked ? "/resources/img/circle-fill.svg" : "/resources/img/circle.svg";
+    });
 });
